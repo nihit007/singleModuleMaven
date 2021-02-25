d18f833eacb23f9024983482725bfd4bd309b630
Nihit Garg <nihit.garg@tothenew.com>
2021-02-25 21:32:24 +0530

    commit done

---
+++ src/main/java/com/firstproject/sysout.java
@@ -0,0 +1,13 @@
+package com.firstproject;
+
+public class sysout {
+
+	public static void main(String[] args) {
+		sysout s1 = new sysout();
+	s1.ret();
+	
+}
+	void ret() {
+		System.out.println("hello world");
+	}
+}
---
+++ src/test/java/com/firstproject/sysoutTest.java
@@ -0,0 +1,14 @@
+package com.firstproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sysoutTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
